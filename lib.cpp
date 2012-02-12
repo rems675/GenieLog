d79b35f5179d12584f9e9ec7822c0faa90b19030
rems675 <remy.cachalot13@free.fr>
2012-02-12 22:37:22 +0100

    dimanche

--- lib.cpp
+++ lib.cpp
@@ -4,7 +4,26 @@
 
 	char* add(char * left, int sizeOfLeft, char * right, int sizeOfRight)
 	{
-		return left;
-
+		int detention=0, number=0;
+		char* result=NULL;
+		
+		bool sign=false; // false represent positif number
+		if(left[0]=='-')
+		sign=true;
+		if(right[0]=='-')
+		//call substration 
+		
+		if(sizeOfLeft>sizeOfRight)
+			 result=new char[sizeOfLeft+2];
+		if(sizeOfLeft<sizeOfRight)
+			result=new char[sizeOfRight+2];
+		else
+		result=new char[sizeOfLeft+2];
+		for(int i =sizeOfLeft-1; i<=0; i--)
+		{
+			number=left[i]+right[i]+detention;
+			result[i]=number%10;
+		}
+		return result;
 	}
 
