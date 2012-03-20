345fb2506c1451f28c7868278be33d1dc9702deb
remy <toto>
2012-03-20 23:27:19 +0100

    documentation et module fichier"

--- lib.cpp
+++ lib.cpp
@@ -374,7 +374,6 @@ char * complement(char * s, int sizeOfLittle, int sizeOfBig) // fonction qui fai
  *
  * \return true si corectement déroulée - sinon false
  */
- //HERE FOR YOU !
 bool fileLoad(char * file)
 {
 	char numberOne[2048];
